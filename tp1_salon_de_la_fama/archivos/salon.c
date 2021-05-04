4b66afe2d6d7b200a329c3cf084a07ed9dcc0e9a
fedecarboni7 <fedecarboni7@gmail.com>
2021-05-04 01:43:17 -0300

    empezamos con salon

--- tp1_salon_de_la_fama/archivos/salon.c
+++ tp1_salon_de_la_fama/archivos/salon.c
@@ -1,7 +1,29 @@
 #include "salon.h"
+#include "util.h"
 #include <stdlib.h>
 
 salon_t* salon_leer_archivo(const char* nombre_archivo){
+    salon_t* salon = malloc(sizeof(salon_t));
+    entrenador_t* entrenador = malloc(sizeof(entrenador_t));
+    pokemon_t* pokemon = malloc(sizeof(pokemon_t));
+    
+    char* linea = fgets_alloc(nombre_archivo);
+    int cantidad_vectores = vtrlen(split(linea, ';'));
+
+    while(linea != NULL){
+        if(cantidad_vectores == 2){
+            char** vector = split(linea, ';');
+            //entrenador -> victorias = atoi(vector[1]);
+        }
+        if(cantidad_vectores == 6){
+            char** vector = split(linea, ';');
+            //pokemon;
+        }
+        linea = fgets_alloc(nombre_archivo);
+        cantidad_vectores = vtrlen(split(linea, ';'));
+    }
+
+    fclosen(nombre_archivo);
     return NULL;
 }
 
