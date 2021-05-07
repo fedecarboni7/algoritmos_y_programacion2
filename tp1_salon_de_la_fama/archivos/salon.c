7943cd72f1039b3aaf68c90b594e2648c57ae00e
fedecarboni7 <fedecarboni7@gmail.com>
2021-05-07 01:02:00 -0300

    avanzamos un poco mas con salon.c

--- tp1_salon_de_la_fama/archivos/salon.c
+++ tp1_salon_de_la_fama/archivos/salon.c
@@ -1,34 +1,57 @@
 #include "salon.h"
 #include "util.h"
 #include <stdlib.h>
+#include <string.h>
 
 salon_t* salon_leer_archivo(const char* nombre_archivo){
-    /* Comentado para evadir errores en make test
-    
     salon_t* salon = malloc(sizeof(salon_t));
+    if(!salon)
+        return NULL;
     entrenador_t* entrenador = malloc(sizeof(entrenador_t));
+    if(!entrenador){
+        free(salon);
+        return NULL;
+    }
     pokemon_t* pokemon = malloc(sizeof(pokemon_t));
-    
+    if(!pokemon){
+        free(salon);
+        free(entrenador);
+        return NULL;
+    }
     char* linea = fgets_alloc(nombre_archivo);
-    int cantidad_vectores = vtrlen(split(linea, ';'));
+    size_t cantidad_vectores = vtrlen(split(linea, ';'));
 
     while(linea != NULL){
-        if(cantidad_vectores == 2){
+        if(cantidad_vectores == 2){//Compruebo que se trate de una línea de entrenador
             char** vector = split(linea, ';');
-            //entrenador -> victorias = atoi(vector[1]);
+            void* aux = realloc(salon->entrenadores, sizeof(entrenador_t));
+            if(!aux){
+                free(salon);
+                free(entrenador);
+                free(pokemon);
+                return NULL;
+            }
+            salon->entrenadores = aux;
+            entrenador -> victorias = atoi(vector[1]);
         }
-        if(cantidad_vectores == 6){
+        if(cantidad_vectores == 6){//Compruebo que se trate de una línea de pokemon
             char** vector = split(linea, ';');
-            //pokemon;
+            void* aux = realloc(entrenador->equipo, sizeof(pokemon_t));
+            if(!aux){
+                free(salon);
+                free(entrenador);
+                free(pokemon);
+                return NULL;
+            }
+            entrenador->equipo = aux;
+            pokemon->defensa = atoi(vector[1]);
         }
         linea = fgets_alloc(nombre_archivo);
         cantidad_vectores = vtrlen(split(linea, ';'));
     }
 
     fclosen(nombre_archivo);
-    return NULL;
-    */
-    return NULL;
+    return salon;
 }
 
 int salon_guardar_archivo(salon_t* salon, const char* nombre_archivo){
@@ -48,5 +71,7 @@ void salon_mostrar_entrenador(entrenador_t* entrenador){
 }
 
 void salon_destruir(salon_t* salon){
-
+    vtrfree(salon);
+    free(salon->entrenadores);
+    free(salon);
 }
