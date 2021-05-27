649fa9faea984aaccca40229f624fc6d764914b0
fedecarboni7 <fedecarboni7@gmail.com>
2021-05-27 01:24:30 -0300

    falta insertar y borrar en posicion, y los iteradores

--- tda_lista/lista.c
+++ tda_lista/lista.c
@@ -32,6 +32,30 @@ int lista_insertar(lista_t* lista, void* elemento){
     return 0;
 }
 
+int lista_insertar_primero(lista_t* lista, void* elemento){
+    if(!lista)
+        return -1;
+    if(!lista->nodo_fin){
+        nodo_t* nuevo_nodo = calloc(1, sizeof(nodo_t));
+        if(!nuevo_nodo)
+            return -1;
+        nuevo_nodo->elemento = elemento;
+        lista->nodo_inicio = nuevo_nodo;
+        lista->nodo_fin = nuevo_nodo;
+        lista->nodo_fin->siguiente = lista->nodo_inicio;
+    }
+    else{
+        nodo_t* nuevo_nodo = calloc(1, sizeof(nodo_t));
+        if(!nuevo_nodo)
+            return -1;
+        nuevo_nodo->elemento = elemento;
+        lista->nodo_inicio->siguiente = nuevo_nodo;
+        lista->nodo_inicio = nuevo_nodo;
+    }
+    lista->cantidad++;
+    return 0;
+}
+
 int lista_insertar_en_posicion(lista_t* lista, void* elemento, size_t posicion){
     return 0;
 }
@@ -39,7 +63,10 @@ int lista_insertar_en_posicion(lista_t* lista, void* elemento, size_t posicion){
 int lista_borrar(lista_t* lista){
     if(!lista)
         return -1;
-    
+    nodo_t* nodo_aux = lista->nodo_inicio;
+    lista->nodo_inicio = lista->nodo_inicio->siguiente;
+    lista->cantidad--;
+    free(nodo_aux);
     return 0;
 }
 
@@ -58,14 +85,28 @@ int lista_borrar_de_posicion(lista_t* lista, size_t posicion){
 }
 
 void* lista_elemento_en_posicion(lista_t* lista, size_t posicion){
-    return NULL;
+    if(!lista)
+        return NULL;
+    if(posicion >= lista->cantidad)
+        return NULL;
+    nodo_t* nodo_aux = lista->nodo_inicio;
+    size_t posicion_aux = 0;
+    while(posicion_aux < posicion){
+        nodo_aux = nodo_aux->siguiente;
+        posicion_aux++;
+    }
+    return nodo_aux->elemento;
 }
 
 void* lista_ultimo(lista_t* lista){
-    return NULL;
+    if(!lista)
+        return NULL;
+    return lista->nodo_fin->elemento;
 }
 
 bool lista_vacia(lista_t* lista){
+    if(lista->cantidad == 0)
+        return true;
     return false;
 }
 
@@ -76,27 +117,43 @@ size_t lista_elementos(lista_t* lista){
 }
 
 int lista_apilar(lista_t* lista, void* elemento){
+    if(!lista)
+        return -1;
+    lista_insertar_primero(lista, elemento);
     return 0;
 }
 
 int lista_desapilar(lista_t* lista){
+    if(!lista)
+        return -1;
+    lista_borrar_primero(lista);
     return 0;
 }
 
 void* lista_tope(lista_t* lista){
-    return NULL;
+    if(lista->cantidad == 0)
+        return NULL;
+    return lista->nodo_inicio->elemento;
 }
 
 int lista_encolar(lista_t* lista, void* elemento){
+    if(!lista)
+        return -1;
+    lista_insertar(lista, elemento);
     return 0;
 }
 
 int lista_desencolar(lista_t* lista){
+    if(!lista)
+        return -1;
+    lista_borrar_primero(lista);
     return 0;
 }
 
 void* lista_primero(lista_t* lista){
-    return NULL;
+    if(lista->cantidad == 0)
+        return NULL;
+    return lista->nodo_inicio->elemento;
 }
 
 void lista_destruir(lista_t* lista){
--- tda_lista/pruebas.c
+++ tda_lista/pruebas.c
@@ -6,17 +6,19 @@ void dadoQueNecesitoUnaLista_siCreoUnaListaNueva_devuelvoUnaListaVacia(){
 
   pa2m_afirmar(nueva_lista->nodo_inicio == NULL, "Se obtuvo una lista con un nodo inicio igual a NULL");
   pa2m_afirmar(nueva_lista->nodo_fin == NULL, "Se obtuvo una lista con un nodo fin igual a NULL");
-  pa2m_afirmar(lista_elementos(nueva_lista) == 0, "Se obtuvo una lista con cantidad igual a 0");
+  pa2m_afirmar(nueva_lista->cantidad == 0, "Se obtuvo una lista con cantidad igual a 0");
 
   lista_destruir(nueva_lista);
 }
 
 void dadaUnaListaVacia_siLeAgregoUnElemento_resultaEnUnaListaConUnElemento(){
+  lista_t* lista_null = NULL;
   lista_t* nueva_lista = lista_crear();
   void* elemento = malloc(sizeof(int));
 
+  pa2m_afirmar(lista_insertar(lista_null, elemento) == -1, "Dada una lista nula, devuelvo -1");
   pa2m_afirmar(lista_insertar(nueva_lista, elemento) == 0, "Se pudo agregar un elemento a la lista");
-  pa2m_afirmar(lista_elementos(nueva_lista) == 1, "Se obtuvo una lista con un elemento");
+  pa2m_afirmar(nueva_lista->nodo_inicio->elemento == elemento && elemento == nueva_lista->nodo_fin->elemento ,"El primer y ultimo elemento de la lista es el elemento que agregué");
 
   free(elemento);
   lista_destruir(nueva_lista);
@@ -25,14 +27,49 @@ void dadaUnaListaVacia_siLeAgregoUnElemento_resultaEnUnaListaConUnElemento(){
 void dadaUnaLista_siAgregoElementos_laListaSeAgranda(){
   lista_t* lista = lista_crear();
   void* elemento = malloc(sizeof(char));
-  lista_insertar(lista, elemento); //Lista con un elemento
-  void* elemento_nuevo = malloc(sizeof(int));
+  lista_insertar(lista, elemento);
 
-  pa2m_afirmar(lista_insertar(lista, elemento_nuevo) == 0, "Se pudo agregar un elemento nuevo a la lista");
+  pa2m_afirmar(lista_insertar(lista, elemento) == 0, "Se pudo agregar un elemento nuevo a la lista");
   pa2m_afirmar(lista_elementos(lista) == 2, "Se obtuvo una lista con dos elementos");
+  pa2m_afirmar(lista_insertar(lista, elemento) == 0, "Se pudo agregar un elemento nuevo a la lista");
+  pa2m_afirmar(lista_elementos(lista) == 3, "Se obtuvo una lista con tres elementos");
 
   free(elemento);
-  free(elemento_nuevo);
+  lista_destruir(lista);
+}
+
+void dadaUnaLista_siPidoElElementoDeUnaPosicion_reciboEseElemento(){
+  lista_t* lista = lista_crear();
+  void* elemento = malloc(sizeof(char));
+  void* elemento1 = malloc(sizeof(int));
+  void* elemento2 = malloc(sizeof(float));
+  lista_insertar(lista, elemento);
+  lista_insertar(lista, elemento1);
+  lista_insertar(lista, elemento2);
+
+  pa2m_afirmar(lista_elemento_en_posicion(lista, 0) == elemento, "El primer elemento de la lista es el primer agregado");
+  pa2m_afirmar(lista_elemento_en_posicion(lista, 1) == elemento1, "El segundo elemento de la lista es el segundo agregado");
+  pa2m_afirmar(lista_elemento_en_posicion(lista, 2) == elemento2, "El tercer elemento de la lista es el tercer agregado");
+
+  free(elemento);
+  free(elemento1);
+  free(elemento2);
+  lista_destruir(lista);
+}
+
+void dadaUnaLista_siLeAgregoElementos_puedoSaberConCuantosElementosCuenta(){
+  lista_t* lista = lista_crear();
+  void* elemento = malloc(sizeof(char));
+
+  pa2m_afirmar(lista_elementos(lista) == 0, "La lista no tiene elementos o no existe");
+  lista_insertar(lista, elemento);
+  pa2m_afirmar(lista_elementos(lista) == 1, "La lista tiene un elemento");
+  lista_insertar(lista, elemento);
+  pa2m_afirmar(lista_elementos(lista) == 2, "La lista tiene dos elementos");
+  lista_insertar(lista, elemento);
+  pa2m_afirmar(lista_elementos(lista) == 3, "La lista tiene tres elementos");
+
+  free(elemento);
   lista_destruir(lista);
 }
 
@@ -43,6 +80,12 @@ int main(){
   pa2m_nuevo_grupo("Pruebas de lista insertar");
   dadaUnaListaVacia_siLeAgregoUnElemento_resultaEnUnaListaConUnElemento();
   dadaUnaLista_siAgregoElementos_laListaSeAgranda();
+
+  pa2m_nuevo_grupo("Pruebas de lista cantidad de elementos");
+  dadaUnaLista_siLeAgregoElementos_puedoSaberConCuantosElementosCuenta();
+
+  pa2m_nuevo_grupo("Pruebas de lista elemento en posición");
+  dadaUnaLista_siPidoElElementoDeUnaPosicion_reciboEseElemento();
 
   return pa2m_mostrar_reporte();
 }
