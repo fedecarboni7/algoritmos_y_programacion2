bb20e91c7e09976ad81edb0ff202fd2cad2cb509
fedecarboni7 <fedecarboni7@gmail.com>
2021-09-20 15:13:08 -0300

    correcciones pr

--- 2C2021-Buchwald/TPS/TP0/tp0.c
+++ 2C2021-Buchwald/TPS/TP0/tp0.c
@@ -16,59 +16,43 @@ void swap(int *x, int *y) {
 
 
 int maximo(int vector[], int n) {
-    if(n == 0)
-        return -1;
+    if (n == 0) return -1;
     int max = vector[0];
-    for(int i = 0; i < n; i++){
-        if(vector[i] > max){
-            max = vector[i];
-        }
+    for (int i = 0; i < n; i++) {
+        if (vector[i] > vector[max])
+            max = i;
     }
-    int j = 0;
-    while(vector[j] != max){
-        j++;
-    }
-    return j;
+    return max;
 }
 
 
 int comparar(int vector1[], int n1, int vector2[], int n2) {
     int n = 0;
-    if(n1 < n2){
+    if (n1 < n2) {
         n = n1; 
     }
     else {
         n = n2;
     }
-    for(int i = 0; i < n; i++){
-        if(vector1[i] < vector2[i]){
+    for (int i = 0; i < n; i++) {
+        if (vector1[i] < vector2[i]) {
             return -1;
         }
-        else if(vector1[i] > vector2[i]){
+        else if (vector1[i] > vector2[i]) {
             return 1;
         }
     }
-    if(n1 < n2){
+    if(n1 < n2) {
         return -1;
     }
-    else if(n2 < n1){
+    else if(n2 < n1) {
         return 1;
     }
     return 0;
 }
 
-/*
 void seleccion(int vector[], int n) {
-    for (int i = 0; i < n - 1; i++){
-        for (int j = i + 1; j < n; j++){
-            if (vector[i] > vector[j])
-                swap(&vector[i], &vector[j]);
-        }
-    }
-}*/
-
-void seleccion(int vector[], int n) {
-    for(int j = 0; j < n - 1; j++){
+    for (int j = 0; j < n - 1; j++) {
         int i_max = maximo(vector, n - j);
         swap(&vector[i_max], &vector[n-1 - j]);
     }
