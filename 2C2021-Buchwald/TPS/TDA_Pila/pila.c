fd5bacb7da24dc8eaaac599ccbab556e8521275f
fedecarboni7 <fedecarboni7@gmail.com>
2021-09-21 17:32:50 -0300

    falta apilar, desapilar y las pruebas

--- 2C2021-Buchwald/TPS/TDA_Pila/pila.c
+++ 2C2021-Buchwald/TPS/TDA_Pila/pila.c
@@ -15,19 +15,20 @@ struct pila {
  * *****************************************************************/
 
 pila_t *pila_crear(void){
-    pila_t *pila = malloc(sizeof(pila_t));
-
+    pila_t *pila = calloc(1, sizeof(pila_t));
     if (pila == NULL) return NULL;
-    
     return pila;
 }
 
 void pila_destruir(pila_t *pila){
-
+    while (!pila_esta_vacia(pila))
+        pila_desapilar(pila);
+    free(pila);
 }
 
 bool pila_esta_vacia(const pila_t *pila){
-
+    if (pila->cantidad == 0) return true;
+    return false;
 }
 
 bool pila_apilar(pila_t *pila, void *valor){
@@ -35,9 +36,11 @@ bool pila_apilar(pila_t *pila, void *valor){
 }
 
 void *pila_ver_tope(const pila_t *pila){
-
+    if (pila_esta_vacia(pila)) return NULL;
+    return pila->datos[:-1];
 }
 
 void *pila_desapilar(pila_t *pila){
+    if (pila_esta_vacia(pila)) return NULL;
 
 }
