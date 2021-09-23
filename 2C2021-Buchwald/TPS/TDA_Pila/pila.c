39bfdd80f7c7f6ed325efed4e615370bd3ca25bc
fedecarboni7 <fedecarboni7@gmail.com>
2021-09-22 23:02:23 -0300

    arreglos e implementaciones

--- 2C2021-Buchwald/TPS/TDA_Pila/pila.c
+++ 2C2021-Buchwald/TPS/TDA_Pila/pila.c
@@ -16,30 +16,37 @@ struct pila {
 pila_t *pila_crear(void){
     pila_t *pila = calloc(1, sizeof(pila_t));
     if (pila == NULL) return NULL;
+    pila->capacidad = 5;
     return pila;
 }
 
 void pila_destruir(pila_t *pila){
     while (!pila_esta_vacia(pila))
         pila_desapilar(pila);
+    free(pila->datos);
     free(pila);
 }
 
 bool pila_esta_vacia(const pila_t *pila){
-    if (pila->cantidad == 0) return true;
-    return false;
+    return pila->cantidad == 0;
 }
 
 bool pila_apilar(pila_t *pila, void *valor){
-
+    pila->datos[pila->cantidad] = valor;
+    pila->cantidad++;
+    if (pila->cantidad == pila->capacidad)
+        pila->capacidad *= 2
+    return true;
 }
 
 void *pila_ver_tope(const pila_t *pila){
     if (pila_esta_vacia(pila)) return NULL;
-    return pila->datos[:-1];
+    return pila->datos[-1];
 }
 
 void *pila_desapilar(pila_t *pila){
     if (pila_esta_vacia(pila)) return NULL;
+    pila->cantidad--;
 
+    return pila->datos[pila->cantidad + 1];
 }
