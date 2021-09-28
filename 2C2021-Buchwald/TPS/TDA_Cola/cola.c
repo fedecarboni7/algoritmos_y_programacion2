926e4d9a19f3687e68b4d043244d410ffe954819
fedecarboni7 <fedecarboni7@gmail.com>
2021-09-28 17:58:52 -0300

    implementacion primitivas

--- 2C2021-Buchwald/TPS/TDA_Cola/cola.c
+++ 2C2021-Buchwald/TPS/TDA_Cola/cola.c
@@ -3,18 +3,27 @@
 
 // Definición del struct cola.
 
-struct cola {
-    
-       // Cantidad de elementos almacenados.
-      // Capacidad del arreglo 'datos'.
-};
+typedef struct cola {
+    nodo_t* nodo_inicio;
+    nodo_t* nodo_fin;
+} cola_t;
+
+// Definición del struct nodo.
+
+typedef struct nodo {
+    void* dato;
+    struct nodo* prox;    
+} nodo_t;
+
 
 /* *****************************************************************
  *                    PRIMITIVAS DE LA COLA
  * *****************************************************************/
 
 cola_t *cola_crear(void) {
-
+    cola_t *cola = malloc(sizeof(cola_t));
+    if (!cola) return NULL;
+    return cola;
 }
 
 void cola_destruir(cola_t *cola, void (*destruir_dato)(void *)) {
@@ -22,17 +31,47 @@ void cola_destruir(cola_t *cola, void (*destruir_dato)(void *)) {
 }
 
 bool cola_esta_vacia(const cola_t *cola) {
-
+    return cola->nodo_inicio == NULL;
 }
 
 bool cola_encolar(cola_t *cola, void *valor) {
-
+    if(!cola) return false;
+    if(!cola->nodo_inicio) {
+        nodo_t* nodo = calloc(1, sizeof(nodo_t));
+        if(!nodo) return false;
+        nodo->dato = valor;
+        cola->nodo_inicio = nodo;
+        cola->nodo_fin = nodo;
+        cola->nodo_inicio->prox = cola->nodo_fin;
+    }
+    else {
+        nodo_t* nodo = calloc(1, sizeof(nodo_t));
+        if(!nodo) return false;
+        nodo->dato = valor;
+        cola->nodo_fin->prox = nodo;
+        cola->nodo_fin = nodo;
+    }
+    return true;
 }
 
 void *cola_ver_primero(const cola_t *cola) {
-
+    if(cola_esta_vacia(cola)) return NULL;
+    return cola->nodo_inicio->dato;
 }
 
 void *cola_desencolar(cola_t *cola) {
+    if(cola_esta_vacia(cola)) return NULL;
+    void* dato_anterior = cola->nodo_inicio->dato;
+    nodo_t* nodo_aux = cola->nodo_inicio;
+    cola->nodo_inicio = cola->nodo_inicio->prox;
+    free(nodo_aux);
+    return dato_anterior;
+}
 
+nodo_t* nodo_crear(void* valor) {
+    nodo_t* nodo = malloc(sizeof(nodo_t));
+    if(!nodo) return NULL;
+    nodo->dato = valor;
+    nodo->prox = NULL;
+    return nodo;
 }
