d3fcdbfd5289f984fcc59d2ffbab86abf0528e82
fedecarboni7 <fedecarboni7@gmail.com>
2021-09-28 22:11:01 -0300

    sigue diciendo que no lo achico

--- 2C2021-Buchwald/TPS/TDA_Pila/pila.c
+++ 2C2021-Buchwald/TPS/TDA_Pila/pila.c
@@ -13,7 +13,7 @@ struct pila {
  *                    PRIMITIVAS DE LA PILA
  * *****************************************************************/
 
-pila_t *pila_crear(void){
+pila_t *pila_crear(void) {
     pila_t *pila = malloc(sizeof(pila_t));
     if (pila == NULL) return NULL;
     pila->cantidad = 0;
@@ -26,46 +26,42 @@ pila_t *pila_crear(void){
     return pila;
 }
 
-void pila_destruir(pila_t *pila){
+void pila_destruir(pila_t *pila) {
     free(pila->datos);
     free(pila);
 }
 
-bool pila_esta_vacia(const pila_t *pila){
+bool pila_esta_vacia(const pila_t *pila) {
     return pila->cantidad == 0;
 }
 
-void *pila_ver_tope(const pila_t *pila){
+void *pila_ver_tope(const pila_t *pila) {
     if (pila_esta_vacia(pila)) return NULL;
     return pila->datos[pila->cantidad - 1];
 }
 
-bool pila_redimensionar(pila_t *pila) {
-    if (pila->cantidad == pila->capacidad)
-        pila->capacidad *= 2;
-    else {
-        pila->capacidad /= 2;
-    }
+bool pila_redimensionar(pila_t *pila, size_t nueva_capacidad) {
+    pila->capacidad = nueva_capacidad;
     pila->datos = realloc(pila->datos, pila->capacidad * sizeof(void*));
     if (pila->datos == NULL) return false;
     return true;
 }
 
-bool pila_apilar(pila_t *pila, void *valor){
+bool pila_apilar(pila_t *pila, void *valor) {
     pila->datos[pila->cantidad] = valor;
     pila->cantidad++;
     if (pila->cantidad == pila->capacidad) {
-        if(!pila_redimensionar(pila))
+        if(!pila_redimensionar(pila, pila->capacidad * 2))
         return false;
     }
     return true;
 }
 
-void *pila_desapilar(pila_t *pila){
+void *pila_desapilar(pila_t *pila) {
     if (pila_esta_vacia(pila)) return NULL;
     pila->cantidad--;
-    if (pila->cantidad * 4 <= pila->capacidad && pila->capacidad >= 20){
-        if(!pila_redimensionar(pila))
+    if (pila->cantidad * 4 <= pila->capacidad && pila->capacidad >= 20) {
+        if(!pila_redimensionar(pila, pila->capacidad / 2))
         return NULL;
     }
     return pila->datos[pila->cantidad];
