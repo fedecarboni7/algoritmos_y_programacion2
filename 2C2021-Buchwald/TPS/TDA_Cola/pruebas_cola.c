e13486700dcd6cd652a2788301900784d60cff70
fedecarboni7 <fedecarboni7@gmail.com>
2021-10-01 16:49:43 -0300

    pruebas cola vacia

--- 2C2021-Buchwald/TPS/TDA_Cola/pruebas_cola.c
+++ 2C2021-Buchwald/TPS/TDA_Cola/pruebas_cola.c
@@ -1,3 +1,4 @@
+#include "cola.h"
 #include "pila.h"
 #include "testing.h"
 #include <stdio.h>
@@ -5,6 +6,15 @@
 static void prueba_cola_vacia(void) {
     printf("INICIO DE PRUEBAS CON PILA VACIA\n");
 
+    cola_t *cola = cola_crear();
+    print_test("Crear cola", cola != NULL);
+    
+    printf("\nCONDICIÓN DE BORDE\n");
+    print_test("La cola está vacía", cola_esta_vacia(cola));
+    print_test("Desencolar una cola recién creada devuelve NULL", cola_desencolar(cola) == NULL);
+    print_test("Ver el primer elemento de una cola recién creada devuelve NULL", cola_ver_primero(cola) == NULL);
+
+    cola_destruir(cola, NULL);
 }
 
 void pruebas_pila_estudiante() {
