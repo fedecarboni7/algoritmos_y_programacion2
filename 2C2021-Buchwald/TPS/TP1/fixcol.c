5f7270f418253751601ad4d399e33e04b3e69071
fedecarboni7 <fedecarboni7@gmail.com>
2021-11-03 22:14:46 -0300

    un poco avanzamos con el tp1

--- 2C2021-Buchwald/TPS/TP1/fixcol.c
+++ 2C2021-Buchwald/TPS/TP1/fixcol.c
@@ -1,12 +1,17 @@
+#define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <stdlib.h>
 
 int abrir_archivo(){
-    FILE* archivo = fopen("archivo.txt", "r" );
+    FILE* archivo;
+    archivo = fopen("archivo.txt", "r" );
     if (!archivo) return -1;
     return 0;
 }
 
+int leer_archivo(FILE* archivo){
+    return int feof(archivo);
+}
 
 int main(){
     int retorno = 0;
